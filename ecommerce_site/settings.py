815d436ae8baf00a3e1cb9c795f890b7e963986d
Tijani <teejayadewale87@gmail.com>
2025-06-01 01:05:17 +0100

    staticccccccc

--- ecommerce_site/settings.py
+++ ecommerce_site/settings.py
@@ -31,7 +31,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'ecm.lagoswebdev.com']
 CART_SESSION_ID = 'cart'
@@ -134,7 +134,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 
 # Directories where Django will look for static files during development
